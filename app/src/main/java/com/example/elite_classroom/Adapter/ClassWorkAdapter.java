83e6dd9499b22e0348cf4d991f26bf3d35ebdf6e
ROHIT AGRAWAL <64243388+rohit1508-gif@users.noreply.github.com>
2021-02-16 10:53:38 +0530

    pop-menu added

--- app/src/main/java/com/example/elite_classroom/Adapter/ClassWorkAdapter.java
+++ app/src/main/java/com/example/elite_classroom/Adapter/ClassWorkAdapter.java
@@ -2,12 +2,16 @@ package com.example.elite_classroom.Adapter;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.elite_classroom.Models.Recycler_Models.ClassWork;
@@ -34,7 +38,29 @@ public class ClassWorkAdapter extends RecyclerView.Adapter<ClassWorkAdapter.View
     @Override
     public void onBindViewHolder(@NonNull ClassWorkAdapter.ViewHolder holder, int position) {
         ClassWork c = list.get(position);
+        holder.b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popupMenu = new PopupMenu(context, view);
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        // TODO Auto-generated method stub
+                        switch (item.getItemId()) {
+                            case R.id.pop_edit:
 
+                                return true;
+                            case R.id.pop_delete:
+
+                                return true;
+                        }
+                        return false;
+                    }
+                });
+                popupMenu.inflate(R.menu.pop_menu);
+                popupMenu.show();
+            }
+        });
     }
 
     @Override
@@ -45,12 +71,14 @@ public class ClassWorkAdapter extends RecyclerView.Adapter<ClassWorkAdapter.View
     public class ViewHolder extends RecyclerView.ViewHolder {
         ImageView iv;
         TextView t,t1;
+        Button b;
         View mview;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             iv = itemView.findViewById(R.id.classwork_image);
             t = itemView.findViewById(R.id.classwork_title);
             t1 = itemView.findViewById(R.id.classwork_description);
+            b = itemView.findViewById(R.id.pop_button);
             mview = itemView;
         }
     }
